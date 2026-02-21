05189a89b1d47137f04be30a26eeff0adbe2c007
Ranaldo Silva <Ronaldofifastreet@gmail.com>
2026-02-21 17:16:25 -0300

    buildteste

--- estacionamento-web/src/lib/supabase.ts
+++ estacionamento-web/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://bzfzklnzqckolvhomuta.supabase.co';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_z87dqo6PQj5hzlbark_s0A_3v7kHE7j';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
